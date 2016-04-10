f52ef349ce32280927f9f70ed48467670c307109
CandyFace <CandyFace18@gmail.com>
2016-04-10 16:58:35 +0200

    Simplified code and added a safety measure

--- Assets/Scripts/ScrollTexture.cs
+++ Assets/Scripts/ScrollTexture.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class ScrollTexture : MonoBehaviour
 {
-	public float waveAmount;
-	public float waveAmount2;
+	public float waveFreq;
+	public float waveFreq2;
 	public float offsetX;
 	public float offsetY;
 	public float offsetX2;
 	public float offsetY2;
-	public float scaleY;
-	public float scaleY2;
 	public float scaleX;
 	public float scaleX2;
+	public float scaleY;
+	public float scaleY2;
 
 	public bool randomOffset;
 	public bool UseAnimation;
@@ -35,15 +35,18 @@ public class ScrollTexture : MonoBehaviour
 
 		//Enable if you want fancy waves, making the texture move up and down.
 		if (enableWaves) {
-			scaleX2 = Mathf.Cos (Time.time) * 0.2f + 0.7f;
-			scaleX = Mathf.Cos (Time.time) * 0.2f + 0.5f;
+
+			if (waveFreq == 0 && waveFreq2 == 0) {
+
+				//safety measure, to ensure a value is set, if inspector has 0.
+				waveFreq = 0.4f;
+				waveFreq2 = 0.5f;
+			}
+			scaleX2 = Mathf.Cos (Time.time) * 0.2f + waveFreq;
+			scaleX = Mathf.Cos (Time.time) * 0.2f + waveFreq2;
 			mat.mainTextureScale = new Vector2 (scaleX, scaleY);
 			renderer.material.SetTextureScale ("_Tex2", new Vector2 (scaleX2, scaleY2));
-		} else {
-//			//Debug.Log ("second");
-//			mat.mainTextureScale = new Vector2 (scaleX, scaleY);
-//			renderer.material.SetTextureScale ("_Tex2", new Vector2 (scaleX2, scaleY2));
-		}
+		} 		
 			
 		//If enabled, cutoff alpha animation will start.
 		if (UseAnimation == true) {
@@ -62,5 +65,4 @@ public class ScrollTexture : MonoBehaviour
 			renderer.material.SetTextureOffset ("_Tex2", new Vector2(offsetX2 * Time.time, offsetY2 * Time.time));
 		}
 	}
-
 }
